872b09c0b3907c9b7a7a42935f1d173b0c5f6d16
Aviion Gibbs <xanthus58@protonmail.com>
2023-02-07 00:08:28 +1030

    Add: Various fish alias commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,26 @@ end
 
 #Custom Alias
 
+#Logs
+alias ufwlog="cat /var/log/ufw.log"
+alias sshlog="journalctl -u ssh"
+
+#Management
+alias update="yay"
+alias uninstall="yay -Rns"
+
+#Programming utils
+alias gacp="git add .; git commit; git push"
+
+#Filesystem
+alias ..="cd .."
+alias mkdir="mkdir -pv"
+alias home="cd ~/"
+
+#Misc
+alias grep="grep --color=auto"
+alias now="date +"%T""
+alias nowdate="date +\"%d-%m-%Y\""
+
 #Init for starship customization
 starship init fish | source
