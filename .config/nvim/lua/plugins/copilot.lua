b9a0e49024b34d067f9230178d563f5e9277b773
Xanthus <Xanthus58@protonmail.com>
2023-02-21 02:10:54 +1030

    Change: Copilot accept bindings

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
 return {
-    'github/copilot.vim',
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    cmd = { "Copilot" },
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      -- Control + P to accept Copilot suggestion
+      vim.cmd([[imap <silent><script><expr> <C-p> copilot#Accept("")]])
+    end,
+  },
 }
