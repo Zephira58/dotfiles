1af081603698ddbe9bcc107a86d14f8879e189db
Xanthus <Xanthus58@protonmail.com>
2023-03-11 20:51:53 +1030

    Add: Custom man command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,37 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function xanman
+        echo "Xans quick and dirty man page for my custom commands"
+        echo ""
+        echo "-Log Commands-"
+        echo "ufwlog: Print all logs created by the uncomplicated firewall"
+        echo "sshlog: Print all of the sshd logs"
+        echo "f2bstatus <optional:jail>: Prints fail2ban information such as banned ips and jails"
+        echo ""
+        echo "-Management-"
+        echo "update: Updates your entire system automaticly."
+        echo "uninstall <package>: Uninstalls the specified package and all of its files"
+        echo "search <package>: Searches the default repos for the spesified package"
+        echo "install <package>: Installs the spesified package"
+        echo "listprograms: Lists all software on the server"
+        echo ""
+        echo "-Utils-"
+        echo "bashup <file>: Allows you to upload a single file for easy sharing"
+        echo ""
+        echo "-Filesystem-"
+        echo "..: Goes one directory above your present working directory"
+        echo "sortsize: Will automaticly sort all files by size reccursivly and print it"
+        echo 'compress <archivename> <directory>: Will use 7zip to automaticly archive the directory with the highest compression level'
+        echo "extract <archive>: Will extract the archive to the current directory"
+        echo ""
+        echo "-Misc-"
+        echo "nowdate: Will print the current date in dd-mm-yyyy format"
+        echo "now: Will print the current time"
+        echo "publicip: Will print your ipv4 adress"
+        echo "privateip: Prints your private ipv4 server adress"
+end
+
 #Custom Alias
 
 #Logs
@@ -11,13 +42,12 @@ alias f2bstatus="sudo fail2ban-client status"
 
 #Management
 alias update="sudo apt update -y; sudo apt full-upgrade -y; sudo apt autoremove -y"
-alias uninstall="sudo apt uninstall"
+alias uninstall="sudo apt purge"
 alias search="sudo apt search"
 alias install="sudo apt install"
 alias listprograms="sudo apt list"
 
-#Programming utils
-alias gacp="git add .; git commit; git push"
+#Utils
 alias bashup="curl bashupload.com -T"
 
 #Filesystem
