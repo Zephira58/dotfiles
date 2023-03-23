a46b462560dd39ae5b6cf3cdcc35b77c7df8dd9a
Xanthus <Xanthus58@protonmail.com>
2023-03-24 02:38:26 +1030

    Add: Neovim/kickstart and Dexios encryption

--- scripts/vps-startup1.3.0.sh
+++ scripts/vps-startup1.3.0.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-
 GREEN='\033[0;32m'  # Set the color code for green
 RESET='\033[0m'     # Reset the color code
 RED='\033[0;31m'    # Sets color code to red
@@ -10,15 +9,15 @@ password="xanthus"
 
 # check if running as root
 if [ "$(id -u)" -ne 0 ]; then
-		   echo "${RED}[xans-startup-script] Please run as root${RESET}" >&2
-		   	      exit 1
+	echo "${RED}[xans-startup-script] Please run as root${RESET}" >&2
+        exit 1
 fi
 
 # enable automatic service restart
 echo "${GREEN}[xans-startup-script] Enabling automatic service restart${RESET}"
 echo "$nrconf{restart} = 'a';" >> /etc/needrestart/needrestart.conf
 
-# update and upgrade the server 
+# update and upgrade the server
 echo "${GREEN}[xans-startup-script] Updating the package list${RESET}"
 apt update -y
 echo "${GREEN}[xans-startup-script] Updating server software${RESET}"
@@ -29,14 +28,12 @@ sudo apt autoremove -y
 # install required packages
 echo "${GREEN}[xans-startup-script] Installing Fish shell${RESET}"
 apt install fish -y
-echo "${GREEN}[xans-startup-script] Installing Exiftool${RESET}"
-apt install exiftool -y
 echo "${GREEN}[xans-startup-script] Installing Docker${RESET}"
 apt install docker.io -y
 echo "${GREEN}[xans-startup-script] Installing 7zip${RESET}"
 apt install 7zip -y
 echo "${GREEN}[xans-startup-script] Installing Rclone${RESET}"
-curl https://rclone.org/install.sh | sudo bash
+apt install rclone -y
 echo "${GREEN}[xans-startup-script] Installing UFW${RESET}"
 apt install ufw -y
 echo "${GREEN}[xans-startup-script] Installing Fail2ban${RESET}"
@@ -58,6 +55,14 @@ apt install neofetch -y
 echo "${GREEN}[xans-startup-script] Installing Borgbackup${RESET}"
 apt install borgbackup -y
 mkdir /mnt/borgbackup -p
+echo "${GREEN}[xans-startup-script] Installing Dexios${RESET}"
+wget https://github.com/brxken128/dexios/releases/download/v8.8.1/dexios-linux-amd64
+chmod +x dexios-linux-amd64
+mv dexios-linux-amd64 /usr/bin/dexios
+echo "${GREEN}[xans-startup-script] Installing Neovim${RESET}"
+apt install neovim -y
+echo "${GREEN}[xans-startup-script] Installing Git${RESET}"
+apt install git -y
 
 # start and enable services
 echo "${GREEN}[xans-startup-script] Starting and enabling Cron${RESET}"
@@ -79,7 +84,6 @@ ufw logging low
 echo "y" | ufw enable
 echo "${GREEN}[xans-startup-script] Adding localdisk to rclone config${RESET}"
 echo "[local]" >> /root/.config/rclone/rclone.conf
-mkdir /root/.config/rclone/ -p
 echo "type = local" >> /root/.config/rclone/rclone.conf
 
 echo "${GREEN}[xans-startup-script] Starting and enabling Fail2ban${RESET}"
@@ -115,6 +119,12 @@ echo "starship init fish | source" >> /home/$username/.config/fish/config.fish
 chmod 664 /home/$username/.config/fish/config.fish
 usermod --shell /usr/bin/fish $username
 
+#configure neovim
+echo "${GREEN}[xans-startup-script] Configuring Neovim with Kickstart${RESET}"
+mkdir /home/$username/nvim/
+git clone https://github.com/nvim-lua/kickstart.nvim /home/$username/nvim/
+
+
 # set ownership of home directory to user
 echo "${GREEN}[xans-startup-script] Setting ownership of home directory${RESET}"
 chown -R $username:$username /home/$username
@@ -133,7 +143,7 @@ interface=$(ip route get 8.8.8.8 | awk '{print $5; exit}')
 public_ip=$(curl https://api.ipify.org)
 local_ip=$(ip addr show $interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -1)
 
-# remind user to update information 
+# remind user to update information
 echo "${GREEN}[xans-startup-script] -Default login credentials-"
 echo "[xans-startup-script] Username: ${RED}$username${GREEN}"
 echo "[xans-startup-script] Password: ${RED}$password${GREEN}"
